1e574f8db2fad41eebdbf08d09ec2546c5d6e601
Arjun Singh <m25mac003@iitj.ac.in>
2026-08-04 17:17:14 +0530

    Time: 6 ms (39.24%) | Memory: 47.2 MB (25.78%) - LeetSync

---
+++ Easy/4107-find-missing-elements/find-missing-elements.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        int currEle = nums[0];
+
+        for(int i = 0 ; i < nums.length ; currEle++, i++){
+            if(currEle < nums[i]){
+                res.add(currEle);
+                i--;
+            }
+        }
+
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
