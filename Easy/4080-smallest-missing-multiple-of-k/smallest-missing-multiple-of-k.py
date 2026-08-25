ee00f58202a97151d7b109001fb0d907e3043ff8
Arjun Singh <m25mac003@iitj.ac.in>
2026-08-25 19:09:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.35%) - LeetSync

---
+++ Easy/4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        x = 0
+        for n in nums:
+            if not (n % k):
+                i = (n // k) - 1
+                x |= 1 << i
+
+        return ((x + 1) & ~x).bit_length() * k
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
